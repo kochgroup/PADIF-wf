139d00df33d5366387e6df8a6c9b7b3625a553e1
Daniel Felipe Victoria Munoz <dvictori@pz-koch12.uni-muenster.de>
2022-06-28 15:57:20 +0200

    shell script

---
+++ padif.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_1.py CHEMBL2883 1xbt.pdb 32
+/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_1.py CHEMBL2061 1fby.pdb 32
+/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_1.py CHEMBL209 1zzz.pdb 32
