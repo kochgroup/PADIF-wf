edd2a157e6d95e40e46c4faacc40b1b81e4e2f3b
Felipe Victoria-Muñoz <danielfvictoriam@gmail.com>
2022-09-06 15:48:23 +0200

    Modify shell script

--- padif.sh
+++ padif.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL1910 /nfs/home/dvictori/Documents/Docks/pdb_files/2z7g.pdb 32
-/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL2664 /nfs/home/dvictori/Documents/Docks/pdb_files/1li4.pdb 32
-/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL3972 /nfs/home/dvictori/Documents/Docks/pdb_files/1njs.pdb 32
-/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL2883 /nfs/home/dvictori/Documents/Docks/pdb_files/2orv.pdb 32
-/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL2061 /nfs/home/dvictori/Documents/Docks/pdb_files/1fby.pdb 32
-/nfs/home/dvictori/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL209 /nfs/home/dvictori/Documents/Docks/pdb_files/1zzz.pdb 32
+~/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL1910 ~/Documents/Docks/pdb_files/2z7g.pdb 32
+~/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL2664 ~/Documents/Docks/pdb_files/1li4.pdb 32
+~/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL3972 ~/Documents/Docks/pdb_files/1njs.pdb 32
+~/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL2883 ~/Documents/Docks/pdb_files/2orv.pdb 32
+~/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL2061 ~/Documents/Docks/pdb_files/1fby.pdb 32
+~/anaconda3/envs/ccdc/bin/python padif_wf_sdf.py CHEMBL209 ~/Documents/Docks/pdb_files/1zzz.pdb 32
