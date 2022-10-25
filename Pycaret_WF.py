cd553d1bd828cc328255871587c8f58bd895828b
Felipe Victoria-Muñoz <danielfvictoriam@gmail.com>
2022-10-25 16:21:55 +0200

    Pycaret last modifications

--- Pycaret_WF.py
+++ Pycaret_WF.py
@@ -1,12 +1,16 @@
 import os 
+import warnings
 import pandas as pd
+import numpy as np
 from sys import argv
 from pycaret.classification import *
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import average_precision_score
 from imblearn.over_sampling import *
 
 ### Useful functions
-def models_pycaret(train_set, test_set):
+def models_pycaret(train_set, test_set, path, directory):
     """
     Create and get metrics for the best 5 Machine Learning models
 
@@ -16,6 +20,8 @@ def models_pycaret(train_set, test_set):
         Values fron train the machine learning models
     test_set: pandas dataframe
         Values for test the machine leraning models
+    path: dir
+        Directory to save models
 
     Return
     ------
@@ -24,27 +30,43 @@ def models_pycaret(train_set, test_set):
     """
     ### Charge info to process
     best_models = setup(data = train_set, target = "activity", session_id = 125, log_experiment = False,
-                    normalize = True, transformation = True, 
-                    ignore_low_variance = True, fold_shuffle=True, silent = True,
-                    fix_imbalance=True, fix_imbalance_method= adasyn1, use_gpu = True,
-                    imputation_type='iterative')
+                    normalize = True, fold_shuffle=True, silent = True,
+                    fix_imbalance=True, fix_imbalance_method= adasyn1)
     ### Create the best models, tune and finalize them
-    top5 = compare_models(n_select=5, sort= "f1", exclude=["dummy"])
-    tuned_top5 = [tune_model(model, optimize= "f1") for model in top5]
-    fin_top5 = [finalize_model(model) for model in tuned_top5]    
+    models = compare_models(
+        sort = "f1", include=["rf", "xgboost", "svm", "mlp"],
+        n_select=7
+    )
+    info = pull()
+    tuned_models = [tune_model(model, optimize= "f1") for model in models]
+    fin_models = [finalize_model(model) for model in tuned_models]
+
+    ### Save the models
+    folder = f"{path}/models/{directory}"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    names = info.index.tolist()
+    for model, id in zip(fin_models, names):
+        save_model(model, f"{folder}/{id}")   
 
     data_model = []
     bal_acc = []
-    for model in fin_top5:
+    prec_recall_auc= []
+    for model in fin_models:
         un_pre = predict_model(model, data= test_set)
         all_metrics = pull()
         data_model.append(all_metrics)
         bal_acc.append(round(balanced_accuracy_score(un_pre["activity"], un_pre["Label"]),4))
+        prec_recall_auc.append(round(average_precision_score(un_pre["activity"], un_pre["Label"]),4))
 
     metrics = pd.concat(data_model).reset_index(drop = True)
     metrics["Balanced accuracy"] = bal_acc
+    metrics["Prec_recall_auc"] = prec_recall_auc
 
     return metrics
+
+### Avoid warnings
+warnings.filterwarnings("ignore")
 
 ### Create a list from diferent folders with PLIFs files
 dir_list = [dir[0] for dir in os.walk(argv[1])]
@@ -58,8 +80,10 @@ adasyn1= ADASYN(sampling_strategy="minority")
 fp_list = ["PADIF", "PADIF2", "PROLIF", "ECIF"]
 
 for row in zip(dir_list, target_lst):
+    metrics = []
     for fp in fp_list:
         df = pd.read_csv(f"{row[0]}/{row[1]}_{fp}.csv", sep = ",", index_col=0)
+        print(f"reading {row[1]}_{fp}")
 
         def act_change(val):
             if str(val) == "Active":
@@ -71,17 +95,21 @@ for row in zip(dir_list, target_lst):
         df.columns = [*df.columns[:-1], "activity"]
         df["activity"] = df["activity"].apply(lambda x: act_change(x))
 
-        data = df.sample(frac=0.95, random_state=786)
-        data_unseen = df.drop(data.index)
+        y = df.pop("activity").to_frame()
+        x = df
 
-        data.reset_index(inplace=True, drop=True)
-        data_unseen.reset_index(inplace=True, drop=True)
+        X_train, X_test, y_train, y_test =train_test_split(
+                x, y,stratify=y, test_size=0.15)
+        data = pd.concat([X_train, y_train], axis=1)
+        data = data.drop(data.columns[0], axis=1)
+        data_unseen = pd.concat([X_test, y_test], axis=1)
+        data_unseen = data_unseen.drop(data_unseen.columns[0], axis=1)
 
-        metrics = []
-        for fp in fp_list:
-            metric = models_pycaret(data, data_unseen)
-            metric["FP"] = fp
-            metrics.append(metric)
+        metric = models_pycaret(data, data_unseen, row[0], fp)
+        metric["FP"] = fp
+        metrics.append(metric)
+        
+        print(f"ending {row[1]}_{fp}")
 
-        table = pd.concat(metrics)
-        table.to_csv(f"{row[0]}/{row[1]}_metrics.csv", sep=",", index= False)
+    table = pd.concat(metrics)
+    table.to_csv(f"{row[0]}/{row[1]}_metrics_f.csv", sep=",", index= False)
