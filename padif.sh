6c24b8be929fc0b6314b8a93005df2b2a7813557
Felipe Victoria-Muñoz <danielfvictoriam@gmail.com>
2022-11-07 10:21:10 +0100

    PADIF for Zinc15

--- padif.sh
+++ padif.sh
@@ -4,5 +4,5 @@
 # ~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL2664 ~/Documents/to_github/PADIF/files/pdb_files/1li4.pdb 32 results
 # ~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL3972 ~/Documents/to_github/PADIF/files/pdb_files/1njs.pdb 32 results
 # ~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL2883 ~/Documents/to_github/PADIF/files/pdb_files/2orv.pdb 32 results
-~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL2061 ~/Documents/to_github/PADIF/files/pdb_files/1fby.pdb 32 results
+# ~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL2061 ~/Documents/to_github/PADIF/files/pdb_files/1fby.pdb 32 results
 ~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL209 ~/Documents/to_github/PADIF/files/pdb_files/1zzz.pdb 32 results
