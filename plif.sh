2295ecebc030703f6820385788df868ce832cdc4
FelPVic <danielfvictoriam@gmail.com>
2025-02-20 16:41:20 +0100

    deleted old file

--- plif.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-~/anaconda3/envs/rdkit/bin/python PLIF_extr.py ~/Documents/Docks/Adenosine_deaminase/ ~/Documents/plifs/
-~/anaconda3/envs/rdkit/bin/python PLIF_extr.py ~/Documents/Docks/Adenosylhomocysteinase/ ~/Documents/plifs/
-~/anaconda3/envs/rdkit/bin/python PLIF_extr.py ~/Documents/Docks/Catechol_O-methyltransferase/ ~/Documents/plifs/
-~/anaconda3/envs/rdkit/bin/python PLIF_extr.py ~/Documents/Docks/GAR_transformylase/ ~/Documents/plifs/
-~/anaconda3/envs/rdkit/bin/python PLIF_extr.py ~/Documents/Docks/Retinoid_X_receptor_alpha/ ~/Documents/plifs/
-~/anaconda3/envs/rdkit/bin/python PLIF_extr.py ~/Documents/Docks/Thymidine_kinase__cytosolic/ ~/Documents/plifs/
-~/anaconda3/envs/rdkit/bin/python PLIF_extr.py ~/Documents/Docks/Trypsin_I/ ~/Documents/plifs/ 
