41132eeb6ccd151f8657ed1067b4c761a5721504
FelPVic <danielfvictoriam@gmail.com>
2025-02-20 16:40:33 +0100

    deleted old file

--- padif.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL1910 ~/Documents/to_github/PADIF/files/pdb_files/2z7g.pdb 32 results
-~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL2664 ~/Documents/to_github/PADIF/files/pdb_files/1li4.pdb 32 results
-~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL3972 ~/Documents/to_github/PADIF/files/pdb_files/1njs.pdb 32 results
-~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL2883 ~/Documents/to_github/PADIF/files/pdb_files/2orv.pdb 32 results
-~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL2061 ~/Documents/to_github/PADIF/files/pdb_files/1fby.pdb 32 results
-~/anaconda3/envs/ccdc/bin/python padif_wf.py CHEMBL209 ~/Documents/to_github/PADIF/files/pdb_files/1zzz.pdb 32 results
