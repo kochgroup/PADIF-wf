c618faa391ade921e855e4155fb7388c8c217069
FelPVic <danielfvictoriam@gmail.com>
2025-02-21 09:50:25 +0100

    first test

--- padif_ml.py
+++ padif_ml.py
@@ -99,9 +99,11 @@ def active_docking(ligands, path_target, target_name):
         shutil.copy(filename, act_f)
 
     ### Copy plp_protein to principal folder
-    shutil.copy(f"{act_dir}/plp_protein.mol2", path_target)    
+    shutil.copy(f"{act_dir}/plp_protein.mol2", path_target)  
 
-def decoys_docking(ligands):
+    return myTemp, act_f  
+
+def decoys_docking(ligands, temp, path_target, target_name):
 
     ### Calculate scaffolds for active compounds
     scaffolds = Parallel(n_jobs=-1)(delayed(gen_scaffold)(smi) for smi in ligands.new_smiles.values)
@@ -115,14 +117,14 @@ def decoys_docking(ligands):
     decoys = decoys_df.iloc[random.sample(range(len(decoys_df_f)), len(ligands)*4)] 
 
     ### make a temp folder and prepare decoys
-    dec_dir = tempfile.mkdtemp(suffix=None,prefix='ina_',dir=myTemp)
+    dec_dir = tempfile.mkdtemp(suffix=None,prefix='ina_',dir=temp)
     decoys['ina_dir'] = [dec_dir]*len(decoys)
     tasks_decoys = decoys.apply(lambda row: (row['smiles'], row['id'], row['ina_dir']), axis=1)
     results = Parallel(n_jobs=-1)(delayed(prep_ligand_from_smiles)(*task) for task in tasks_decoys) 
 
     ### Dock the Active compounds
     to_dock_decoys = decoys[['id', 'ina_dir']]
-    to_dock_decoys['gold_file'], to_dock_decoys['num_sln'] = [f'{path_target}/{targetName}_gold.conf']*len(to_dock_decoys), [10]*len(to_dock_decoys)
+    to_dock_decoys['gold_file'], to_dock_decoys['num_sln'] = [f'{path_target}/{target_name}_gold.conf']*len(to_dock_decoys), [10]*len(to_dock_decoys)
     tasks_dock_decoys = to_dock_decoys.apply(lambda row: (row['gold_file'], row['id'], row['ina_dir'], row['num_sln']), axis=1)
     results_dock_decoys = Parallel(n_jobs=-1)(delayed(dock_mol)(*task) for task in tasks_dock_decoys)   
 
@@ -134,10 +136,40 @@ def decoys_docking(ligands):
         shutil.copy(filename, dec_f)    
 
     shutil.copy(f"{dec_dir}/plp_protein.mol2", path_target)
-    ### Remove Bad files
-    gc.collect()
-    shutil.rmtree(myTemp)
 
+    return dec_f
+
+def padif_extraction(ligands, active_folder, decoy_folder, path_target, target_name):
+
+    ### Extract PADIF for actives compounds and add the SMILES for each molecule
+    active_padif = padif_to_dataframe(active_folder, cavity_file=f'{path_target}/cavity.atoms', actives=True)
+    ligands_info = ligands[['id','new_smiles']]
+    active_padif = active_padif.merge(ligands_info, on='id')
+    active_padif = active_padif.rename(columns={'new_smiles':'smiles'})
+    ### Extract PADIF for decoys compounds and add the SMILES for each molecule
+    decoys_padif = padif_to_dataframe(decoy_folder, cavity_file=f'{path_target}/cavity.atoms')
+    decoys_info = decoys[['id','smiles']]
+    decoys_padif = decoys_padif.merge(decoys_info, on='id')
+    ### concat the list and save PADIF file
+    final_padif = pd.concat([active_padif, decoys_padif], ignore_index=True)
+    final_padif = final_padif.fillna(0.0)
+    ### Sort columns and put in the end of dataframe for better visualization
+    to_move = ['score', 'id', 'smiles','activity']
+    order_1 =[col for col in final_padif.columns if col not in to_move] + to_move
+    final_padif = final_padif[order_1]
+    final_padif.to_csv(f'{path_target}/{target_name}_PADIF.csv', sep=',', index=False)
+
+def split_datasets(splitters, path_target, target_name):
+
+    data_to_model = os.path.join(path_target, 'data_to_model')
+    os.makedirs(data_to_model, exist_ok=True)
+    ### Split with all types of splitters
+    for splitter in splitters:
+        spl_folder = os.path.join(data_to_model, splitter)
+        os.makedirs(spl_folder, exist_ok=True)
+        path = f'{data_to_model}/{splitter}'
+        to_split(target=target_name, padif_folder=path_target, path_to_work=path, method=splitter)
+   
 
 def main(chembl_code, protein_file, ligand_id):
 
@@ -162,113 +194,58 @@ def main(chembl_code, protein_file, ligand_id):
 
     protein_process(protein_f, ligand_id, path_target, target_name)
 
-    actives_docking(ligands, path_target, target_name)
+    temp, active_folder = actives_docking(ligands, path_target, target_name)
     
+    decoy_folder = decoys_docking(ligands, temp, path_target, target_name)
+
+    padif_extraction(ligands, active_folder, decoy_folder, path_taget, target_name)
+
+    splitters = ['random', 'scaffold', 'fingerprint']
+
+    split_datasets(splitters, path_target, target_name)
+
+    ### Train models
+    padif_train(target_name, splitters, path_target)
+
+    ### Save figures and statistics
+    metrics_df, predictions_df = predictions_models(splitters, path_target)
+    figures_from_metrics(predictions_df, metrics_df, path_target)
+
+    metrics_df.to_csv(f'{path_target}/metrics_of_models.csv', sep=',', index=False)
+    predictions_df.to_csv(f'{path_target}/predictions_of_models.csv', sep=',', index=False)
+
+    ### Remove Bad files
+    gc.collect()
+    shutil.rmtree(temp)
 
 
 
 
-print(
-    f'''    
-    ********************************************************************************
-    Docking time process for {targetName} 
 
-    Time CPU: {round(time.process_time() - start_cpu, 4)}
-    Time clock: {round((time.time() - start_clock)/60,4)} min
-    
-    ********************************************************************************
-    '''
-)
+# #%%
+# ### Save all atoms info and display in chimera
 
-### PADIF extraction
-### Extract PADIF for actives compounds and add the SMILES for each molecule
-active_padif = padif_to_dataframe(act_f, cavity_file=f'{path_target}/cavity.atoms', actives=True)
-ligands_info = ligands[['id','new_smiles']]
-active_padif = active_padif.merge(ligands_info, on='id')
-active_padif = active_padif.rename(columns={'new_smiles':'smiles'})
-### Extract PADIF for decoys compounds and add the SMILES for each molecule
-decoys_padif = padif_to_dataframe(dec_f, cavity_file=f'{path_target}/cavity.atoms')
-decoys_info = decoys[['id','smiles']]
-decoys_padif = decoys_padif.merge(decoys_info, on='id')
-### concat the list and save PADIF file
-final_padif = pd.concat([active_padif, decoys_padif], ignore_index=True)
-final_padif = final_padif.fillna(0.0)
-### Sort columns and put in the end of dataframe for better visualization
-to_move = ['score', 'id', 'smiles','activity']
-order_1 =[col for col in final_padif.columns if col not in to_move] + to_move
-final_padif = final_padif[order_1]
-final_padif.to_csv(f'{path_target}/{targetName}_PADIF.csv', sep=',', index=False)
-
-### Split datasets
-### Define splitters and create folders
-splitters = ['random', 'scaffold', 'fingerprint']
-data_to_model = os.path.join(path_target, 'data_to_model')
-os.makedirs(data_to_model, exist_ok=True)
-### Split with all types of splitters
-for splitter in splitters:
-    spl_folder = os.path.join(data_to_model, splitter)
-    os.makedirs(spl_folder, exist_ok=True)
-    path = f'{data_to_model}/{splitter}'
-    to_split(target=targetName, padif_folder=path_target, path_to_work=path, method=splitter)
-
-
-### Train models
-padif_train(targetName, splitters, path_target)
-
-print(
-    f'''    
-    ********************************************************************************
-    Models training time process for {targetName} 
-
-    Time CPU: {round(time.process_time() - start_cpu, 4)}
-    Time clock: {round((time.time() - start_clock)/60,4)} min
-    
-    ********************************************************************************
-    '''
-)
-
-### Save figures and statistics
-metrics_df, predictions_df = predictions_models(splitters, path_target)
-figures_from_metrics(predictions_df, metrics_df, path_target)
-
-metrics_df.to_csv(f'{path_target}/metrics_of_models.csv', sep=',', index=False)
-predictions_df.to_csv(f'{path_target}/predictions_of_models.csv', sep=',', index=False)
-
-print(
-    f'''    
-    ********************************************************************************
-    All time process for {targetName} 
-
-    Time CPU: {round(time.process_time() - start_cpu, 4)}
-    Time clock: {round((time.time() - start_clock)/60,4)} min
-    
-    ********************************************************************************
-    '''
-)
-#%%
-### Save all atoms info and display in chimera
-
-### Extract information about protein and atoms by activity
-protein_df = atom_interaction_info(path_target)
-act, dec, all = atoms_by_activity(final_padif, protein_df)
-### Create folder to save residue and atom information
-atoms_path = os.path.join(path_target, 'atoms_data')
-os.makedirs(atoms_path, exist_ok=True)
-### Save information about residue
-res_info = residue_interactions(final_padif, atoms_path)
-res_info.to_csv(f'{atoms_path}/residue_information.csv', sep=',', index=False)
-### Create command file and start chimera
-# Path to the command file you want to create
-command_file_path = f"{atoms_path}/setup_chimera.cmd"
-# Create the Chimera command file
-write_chimera_commands(
-     f'{path_target}/{targetName}_prep.mol2',
-     list(all),
-     list(act),
-     list(dec),
-     f'{atoms_path}',
-     command_file_path
-)
-### Start process
-ch_path = '/appl/UCSF/Chimera64-1.17.3/bin/chimera'
-subprocess.run([ch_path, command_file_path])
+# ### Extract information about protein and atoms by activity
+# protein_df = atom_interaction_info(path_target)
+# act, dec, all = atoms_by_activity(final_padif, protein_df)
+# ### Create folder to save residue and atom information
+# atoms_path = os.path.join(path_target, 'atoms_data')
+# os.makedirs(atoms_path, exist_ok=True)
+# ### Save information about residue
+# res_info = residue_interactions(final_padif, atoms_path)
+# res_info.to_csv(f'{atoms_path}/residue_information.csv', sep=',', index=False)
+# ### Create command file and start chimera
+# # Path to the command file you want to create
+# command_file_path = f"{atoms_path}/setup_chimera.cmd"
+# # Create the Chimera command file
+# write_chimera_commands(
+#      f'{path_target}/{targetName}_prep.mol2',
+#      list(all),
+#      list(act),
+#      list(dec),
+#      f'{atoms_path}',
+#      command_file_path
+# )
+# ### Start process
+# ch_path = '/appl/UCSF/Chimera64-1.17.3/bin/chimera'
+# subprocess.run([ch_path, command_file_path])
